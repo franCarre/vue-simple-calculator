1363d6749ad885c715ebb1ae3315b983fc5d7060
franCarre <56691795+franCarre@users.noreply.github.com>
2020-07-27 18:18:56 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <a @click="themeSwitch" id="switch"><i class="fas fa-moon"></i></a>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  },
+  methods: {
+    darkTheme() {
+      let darkTheme = document.getElementById("light-theme");
+      darkTheme.setAttribute("href", "/css/dark.css");
+      darkTheme.setAttribute("id", "dark-theme");
+      document.querySelector('#switch').innerHTML = '<i class="fas fa-sun"></i>';
+    },
+    lightTheme() {
+      let lightTheme = document.getElementById("dark-theme");
+      lightTheme.setAttribute("href", "/css/light.css");
+      lightTheme.setAttribute("id", "light-theme");
+      document.querySelector('#switch').innerHTML = '<i class="fas fa-moon"></i>';
+    },
+    themeSwitch() {
+      let darkTheme = document.querySelector("#dark-theme");
+      if (!darkTheme) {
+        this.darkTheme()
+      } else {
+        this.lightTheme()
+      }
+    }
+  }
+}
+</script>
